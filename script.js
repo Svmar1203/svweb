dde69bd7bb3fc7f6d0517ecb3c590bae085b63cb
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-02-08 21:53:53 +0200

    08.02.22

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
 const mouseCircleFn = (x, y) => {
-  mouseCircle.style.cssText = `top: ${y}px; left:${x}px`;
-  mouseDot.style.cssText = `top: ${y}px; left:${x}px`;
+  mouseCircle.style.cssText = `top: ${y}px; left:${x}px; opacity: 1`;
+  mouseDot.style.cssText = `top: ${y}px; left:${x}px; opacity: 1`;
 };
 
 document.body.addEventListener("mousemove", (e) => {
@@ -11,4 +11,9 @@ document.body.addEventListener("mousemove", (e) => {
   let y = e.clientY;
 
   mouseCircleFn(x, y);
+});
+
+document.body.addEventListener("mouseleave", () => {
+  mouseCircle.style.opacity = "0";
+  mouseDot.style.opacity = "0";
 });
