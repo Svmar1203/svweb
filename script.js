d4156826868523301d3d0e49dc5f22787ad2c5dc
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-02-11 23:35:50 +0200

    11.02.22

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const mouseCircleFn = (x, y) => {
   mouseDot.style.cssText = `top: ${y}px; left:${x}px; opacity: 1`;
 };
 
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+const animateCircles = (e, x, y) => {
+  mX = e.clientX;
+  mY = e.clientY;
+};
+
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
