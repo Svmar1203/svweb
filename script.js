20440592e850cd8705161d6d15ec3431755ba37c
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-02-17 22:49:14 +0200

    17.02.22

--- script.js
+++ script.js
@@ -88,3 +88,16 @@ Array.from(aboutMeTextContent).forEach((char) => {
     e.target.style.animation = "aboutMeTextAnim 10s infinite";
   });
 });
+
+const projects = document.querySelectorAll(".project");
+projects.forEach((project) => {
+  project.addEventListener("mouseenter", () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
+
+  project.addEventListener("mouseleave", () => {
+    project.firstElementChild.style.top = "2rem";
+  });
+});
